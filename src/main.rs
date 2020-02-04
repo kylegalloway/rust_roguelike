657661f47f035b38c242b4f33c922478afb4dc5b
Kyle Galloway <kyle.galloway.1993@gmail.com>
2020-02-04 09:05:33 -0600

    Chapter 5: Finished

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ extern crate specs_derive;
 mod components;
 mod map;
 mod player;
-// mod rect;
+mod rect;
+mod visibility_system;
 pub use components::*;
 pub use map::*;
 use player::*;
-// pub use rect::Rect;
+pub use rect::Rect;
+use visibility_system::VisibilitySystem;
 
 pub struct State {
     ecs: World,
@@ -18,6 +20,8 @@ pub struct State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem{};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -29,8 +33,7 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -47,21 +50,28 @@ fn main() {
         .with_title("Roguelike Tutorial")
         .build();
     let mut gs = State { ecs: World::new() };
+
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
 
-    gs.ecs.insert(new_map_test());
+    // The World Map
+    let map : Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
+    gs.ecs.insert(map);
 
+    // The Player
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position { x: player_x, y: player_y })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(Viewshed{ visible_tiles : Vec::new(), range : 8, dirty: true })
         .build();
 
     rltk::main_loop(context, gs);
